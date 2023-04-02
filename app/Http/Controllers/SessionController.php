bb469f740ed18b6b60e4ece2a692a6678a535f4e
alimurrofid <alimurrofid77@gmail.com>
2023-04-02 19:23:33 +0700

    menambahkan fitur registrasi

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
 class SessionController extends Controller
@@ -39,10 +41,58 @@ class SessionController extends Controller
             return redirect('sesi')->withErrors('Email atau Password salah');
         }
     }
-    
+
     function logout ()
     {
         Auth::logout();
         return redirect('sesi')->with('success', 'Logout berhasil');
+    }
+
+    function register()
+
+    {
+        return view('sesi/register');
+    }
+    function create(Request $request)
+    {
+        Session::flash('name', $request->name);
+        Session::flash('email', $request->email);
+        //proses validasi
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ],[
+            'name.required' => 'Nama tidak boleh kosong',
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Email tidak valid',
+            'email.unique' => 'Email sudah terdaftar',
+            'password.required' => 'Password tidak boleh kosong',
+            'password.min' => 'Password minimal 6 karakter'
+        ]);
+
+        //proses registrasi
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password) 
+        ];
+
+        //memasukan data
+         User::create($data);
+
+        //proses otentikasi
+        $infologin = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (Auth::attempt($infologin)) {
+            //kalau otentikasi sukses
+            return redirect('siswa')->with('success', Auth::user()->name. 'Login berhasil');
+        } else {
+            //kalau otentikasi gagal
+            return redirect('sesi')->withErrors('Email atau Password yang dimasukan tidak valid');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::get('/sesi', [SessionController::class, 'index']);
 Route::post('/sesi/login', [SessionController::class, 'login']);
 Route::get('/sesi/logout', [SessionController::class, 'logout']);
 
+Route::get('/sesi/register', [SessionController::class, 'register']);
+Route::post('/sesi/create', [SessionController::class, 'create']);
+
